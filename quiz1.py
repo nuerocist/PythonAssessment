9b3efd3ff3b1241f07c8ebb89bad97a85c46b9f4
aryanjoshi <127735906+nuerocist@users.noreply.github.com>
2025-04-09 20:33:10 +1200

    Update and rename quiztest.py to quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -1,4 +1,16 @@
+# Quiz 1
+# Terminal Based Quiz
+
+name = input("Enter your name: ")
+year_group = int(input("Enter your year group: "))
+if year_group < 8:
+    print("You are too young to take the quiz")
+    exit()
+else:
+    print("Welcome to the quiz")
+632,
 score = 0
+
 qa = {
     'NZ': 'WGTN',
     'AUS': 'CAN',
